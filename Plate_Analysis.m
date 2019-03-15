f516448d62227f552e4c6dd0eab0c37ebc8380a9
veralee98 <47610669+veralee98@users.noreply.github.com>
2019-03-15 01:19:26 -0400

    Add files via upload

--- Plate_Analysis.m
+++ Plate_Analysis.m
@@ -103,11 +103,11 @@ line2 = plateR2(ceil(size(plateR2,1)/2), :, :);
 line3 = permute(plateR3(:, ceil(size(plateR3,2)/2), :), [2,1,3]);
 
 %first let's look at all of the frames of the video
-close all
-for i = 1:25
-    figure
-    imshow(plateR1(:,:,i));
-end
+% close all
+% for i = 1:25
+%     figure
+%     imshow(plateR1(:,:,i));
+% end
 
 %plot the line segments to see what they look like
 %THIS MAKES 25 FIGURES GET READY
@@ -134,194 +134,13 @@ for i = 1:25
 end
 %}
 
-
-%% pixel selection method of calculating edge distance
-%How it works: user will be given 9 images on which they will select a
-%pixel they think represents the edge of the GFP. The average of these
-%pixel intensities will be taken as the absolute threshold for brightness.
-%I will be using "normalized" images in which the first frame is subtracted
-%from all the images
-
-%Background subtraction - subtract the first frame from all frames keeping
-%the values minimum 0
-for i = 1:25
-    plateR1_norm(:,:,i) = plateR1(:,:,i) - plateR1(:,:,1);
-    plateR1_norm(plateR1_norm < 0) = 0; 
-    plateR2_norm(:,:,i) = plateR2(:,:,i) - plateR2(:,:,1);
-    plateR2_norm(plateR2_norm < 0) = 0;
-    plateR3_norm(:,:,i) = plateR3(:,:,i) - plateR3(:,:,1);
-    plateR3_norm(plateR3_norm < 0) = 0;
-end
-
-%lets look at it
-%{
-close all
-for i = 1:25
-    figure
-    imshow(plateR2_norm(:,:,i));
-end
-%}
-
-
-%plot 9 images, 3 from each normalized plate and select a pixel where you
-%think the edge distance is
-% for plate 1 i will use images 6, 9, and 12
-%for plate 2 I will use 16, 20, 24
-% for plate 3 I will use 16, 20, 24
-pixvals = NaN(1,9);
-for i = 1 : 9
-    if i < 4
-        f = figure;
-        index = 3 + 3*i;
-        imshow(plateR1_norm(:,:,index));
-        [x,y]=ginput(1);
-        close all
-        pixel_x=round(x);
-        pixel_y= round(y);
-        pixvals(i) = plateR1_norm(pixel_y,pixel_x,index);
-    elseif i > 3 && i < 7
-        f = figure;
-        index = 12 + 4*(i-3);
-        imshow(plateR2_norm(:,:,index));
-        [x,y]=ginput(1);
-        close all
-        pixel_x=round(x);
-        pixel_y= round(y);
-        pixvals(i) = plateR2_norm(pixel_y,pixel_x,index);
-    else
-        f = figure;
-        index = 12 + 4*(i-6);
-        imshow(plateR3_norm(:,:,index));
-        [x,y]=ginput(1);
-        close all
-        pixel_x=round(x);
-        pixel_y= round(y);
-        pixvals(i) = plateR3_norm(pixel_y,pixel_x,index);
-    end
-end
-
-%set the threshold
-abs_thresh = mean(pixvals);
-sd_thresh = std(pixvals);
-
-%now create line segments across the centers of the normalized plates
-line1_norm = permute(plateR1_norm(:, ceil(size(plateR1_norm,2)/2), :), [2,1,3]);
-line2_norm = plateR2_norm(ceil(size(plateR2_norm,1)/2), :, :);
-line3_norm = permute(plateR3_norm(:, ceil(size(plateR3_norm,2)/2), :), [2,1,3]);
-
-%now save the first and last points where the intensity is greater than the
-%threshold across each line segment
-line1_hidist = NaN(1,25);
-line1_lodist = NaN(1,25);
-line2_hidist = NaN(1,25);
-line2_lodist = NaN(1,25);
-line3_hidist = NaN(1,25);
-line3_lodist = NaN(1,25);
-
-%create a matrix to index into relating distances from center to indices
-%I'll just make these negative to positive but then take the abs() when I
-%use
-distances1 = linspace(-30, 30, length(line1_norm));
-distances2 = linspace(-30, 30, length(line2_norm));
-distances3 = linspace(-30, 30, length(line3_norm));
-
-for i = 1:25
-    %find where pixel is greater than threshold
-    firstind = find(line1_norm(:,:,i) > abs_thresh, 1, 'first');
-    lastind = find(line1_norm(:,:,i) > abs_thresh, 1, 'last');
-    %check if they're empty
-    if isempty(firstind)
-        firstval = 0;
-    else 
-        firstval = abs(distances1(firstind));
-    end
-    if isempty(lastind)
-        lastval = 0;
-    else
-        lastval = abs(distances1(lastind));
-    end
-    %now set the lower and upper values accordingly
-    line1_hidist(i) = max([firstval, lastval]);
-    line1_lodist(i) = min([firstval, lastval]);
-    
-    %rinse and repeat
-    
-    %find where pixel is greater than threshold
-    firstind = find(line2_norm(:,:,i) > abs_thresh, 1, 'first');
-    lastind = find(line2_norm(:,:,i) > abs_thresh, 1, 'last');
-    %check if they're empty
-    if isempty(firstind)
-        firstval = 0;
-    else 
-        firstval = abs(distances2(firstind));
-    end
-    if isempty(lastind)
-        lastval = 0;
-    else
-        lastval = abs(distances2(lastind));
-    end
-    %now set the lower and upper values accordingly
-    line2_hidist(i) = max([firstval, lastval]);
-    line2_lodist(i) = min([firstval, lastval]);
-    
-    %find where pixel is greater than threshold
-    firstind = find(line3_norm(:,:,i) > abs_thresh, 1, 'first');
-    lastind = find(line3_norm(:,:,i) > abs_thresh, 1, 'last');
-    %check if they're empty
-    if isempty(firstind)
-        firstval = 0;
-    else 
-        firstval = abs(distances3(firstind));
-    end
-    if isempty(lastind)
-        lastval = 0;
-    else
-        lastval = abs(distances3(lastind));
-    end
-    %now set the lower and upper values accordingly
-    line3_hidist(i) = max([firstval, lastval]);
-    line3_lodist(i) = min([firstval, lastval]); 
-end
-
-%plot to see what it looks like
-close all
-time = 0:24;
-figure
-subplot(3,1,1)
-plot(time, line1_lodist)
-hold on
-plot(time, line1_hidist)
-hold off
-legend("low", "high", 'Location', 'northwest')
-ylim([0 30])
-title("R1")
-set(gca, 'FontSize', 14)
-subplot(3,1,2)
-plot(time, line2_lodist)
-hold on
-plot(time, line2_hidist)
-hold off
-legend("low", "high", 'Location', 'northwest')
-title("R2")
-ylim([0 30])
-ylabel("Distance (mm)")
-set(gca, 'FontSize', 14)
-subplot(3,1,3)
-plot(time, line3_lodist)
-hold on
-plot(time, line3_hidist)
-hold off
-legend("low", "high", 'Location', 'northwest')
-title("R3")
-ylim([0 30])
-xlabel("Time (hr)")
-set(gca, 'FontSize', 14)
-
-%lastly save the data
-
 %% Multiple line segment selection - second way of doing it
 %selects 4 line segments along each of the cardinal axes - then calculate 8
 %edge distances based on these distances
+
+% for i = 2:25
+%     plateR1(:, :, i) = plateR1(:, :, i)-plateR1(:, :, 1);
+% end
 
 %calculate the vertical and horixontal line segments, orienting them from
 %left -> right center -> outwards. All matrices will start with the center
@@ -346,3 +165,73 @@ plateR3_w = flip(plateR3(radii(2) + 1, 1:radii(2) + 1, :));
 xgrid1 = linspace(0, 30, size(plateR1_n,2));
 xgrid2 = linspace(0, 30, size(plateR2_n,2));
 xgrid3 = linspace(0, 30, size(plateR3_n,2));
+
+for i = 1:25
+plateR1_e(1, :, i) = smoothdata(plateR1_e(1, :, i));
+plateR1_n(1, :, i) = smoothdata(plateR1_n(1, :, i));
+plateR1_w(1, :, i) = smoothdata(plateR1_w(1, :, i));
+plateR1_s(1, :, i) = smoothdata(plateR1_s(1, :, i));
+
+% plateR2_e(1, :, i) = smoothdata(plateR2_e(1, :, i));
+% plateR2_n(1, :, i) = smoothdata(plateR2_n(1, :, i));
+% plateR2_w(1, :, i) = smoothdata(plateR2_w(1, :, i));
+% plateR2_s(1, :, i) = smoothdata(plateR2_s(1, :, i));
+% 
+% plateR3_e(1, :, i) = smoothdata(plateR3_e(1, :, i));
+% plateR3_n(1, :, i) = smoothdata(plateR3_n(1, :, i));
+% plateR3_w(1, :, i) = smoothdata(plateR3_w(1, :, i));
+% plateR3_s(1, :, i) = smoothdata(plateR3_s(1, :, i));
+end
+
+%% Calculate edge distance
+
+edge_distanceR1 = zeros(1, 25);
+edge_distanceR2 = zeros(1, 25);
+edge_distanceR3 = zeros(1, 25);
+
+%first edge distance is 0
+edge_distanceR1e(1) = 0;
+edge_distanceR1n(1) = 0;
+edge_distanceR1w(1) = 0;
+edge_distanceR1s(1) = 0;
+edge_distanceR2e(1) = 0;
+edge_distanceR2n(1) = 0;
+edge_distanceR2w(1) = 0;
+edge_distanceR2s(1) = 0;
+edge_distanceR3e(1) = 0;
+edge_distanceR3n(1) = 0;
+edge_distanceR3w(1) = 0;
+edge_distanceR3s(1) = 0;
+
+for i = 2:25
+    sec_deriv_R1e = gradient(gradient(plateR1_e(1, :, i)));
+    edge_distanceR1e(i) = xgrid1(sec_deriv_R1e == max(sec_deriv_R1e));
+    sec_deriv_R1n = gradient(gradient(plateR1_n(1, :, i)));
+    edge_distanceR1n(i) = xgrid1(sec_deriv_R1n == max(sec_deriv_R1n));
+    sec_deriv_R1w = gradient(gradient(plateR1_w(1, :, i)));
+    edge_distanceR1w(i) = xgrid1(sec_deriv_R1w == max(sec_deriv_R1w));
+    sec_deriv_R1s = gradient(gradient(plateR1_s(1, :, i)));
+    edge_distanceR1s(i) = xgrid1(sec_deriv_R1s == max(sec_deriv_R1s));
+    
+%     sec_deriv_R2e = gradient(gradient(plateR2_e(1, :, i)));
+%     edge_distanceR2e(i) = xgrid2(sec_deriv_R2e == max(sec_deriv_R2e));
+%     sec_deriv_R2n = gradient(gradient(plateR2_n(1, :, i)));
+%     edge_distanceR2n(i) = xgrid2(sec_deriv_R2n == max(sec_deriv_R2n));
+%     sec_deriv_R2w = gradient(gradient(plateR2_w(1, :, i)));
+%     edge_distanceR2w(i) = xgrid2(sec_deriv_R2w == max(sec_deriv_R2w));
+%     sec_deriv_R2s = gradient(gradient(plateR2_s(1, :, i)));
+%     edge_distanceR2s(i) = xgrid2(sec_deriv_R2s == max(sec_deriv_R2s));
+%         
+%     sec_deriv_R3e = gradient(gradient(plateR3_e(1, :, i)));
+%     edge_distanceR3e(i) = xgrid3(sec_deriv_R3e == max(sec_deriv_R3e));
+%     sec_deriv_R3n = gradient(gradient(plateR3_n(1, :, i)));
+%     edge_distanceR3n(i) = xgrid3(sec_deriv_R3n == max(sec_deriv_R3n));
+%     sec_deriv_R3w = gradient(gradient(plateR3_w(1, :, i)));
+%     edge_distanceR3w(i) = xgrid3(sec_deriv_R3w == max(sec_deriv_R3w));
+%     sec_deriv_R3s = gradient(gradient(plateR3_s(1, :, i)));
+%     edge_distanceR3s(i) = xgrid3(sec_deriv_R3s == max(sec_deriv_R3s));
+end
+
+edge_distanceR1 = (edge_distanceR1e+edge_distanceR1n+edge_distanceR1w+edge_distanceR1s)/4;
+%edge_distanceR2 = (edge_distanceR2e+edge_distanceR2w+edge_distanceR2s)/4;
+%edge_distanceR3 = (edge_distanceR3n+edge_distanceR3w+edge_distanceR3s)/4;
